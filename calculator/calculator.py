5f7f2f69bf287ffbe9211c4f27b584dfc985809e
Roger Miguel Staudt <rogermiguelstaudt@gmail.com>
2025-08-04 20:40:08 -0300

    calculator

---
+++ calculator/calculator.py
@@ -0,0 +1,36 @@
+# https://github.com/programiz/Calculator/blob/master/calc.py
+
+import tkinter as tk
+
+LARGE_FONT_STYLE = ("Arial", 40, "bold")
+SMALL_FONT_STYLE = ("Arial", 16)
+DIGITS_FONT_STYLE = ("Arial", 24)
+DEFAULT_FONT_STYLE = ("Arial", 20)
+
+OFF_WHITE = "#F8FAFF"
+WHITE = "#FFFFFF"
+LIGHT_BLUE = "#CCEDFF"
+LIGHT_GRAY = "#F5F5F5"
+LABEL_COLOR = "#25265E"
+
+class Calculator:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry("375x667")
+        self.window.resizable(0, 0)
+        self.window.title("Calculator")
+
+        self.total_expression = ""
+        self.current_expression = ""
+        self.display_frame = self.create_display_frame()
+
+        self.total_label, self.label = self.create_display_labels()
+
+        self.digits = {
+            7: (1, 1), 8: (1, 2), 9: (1, 3),
+            4: (2, 1), 5: (2, 2), 6: (2, 3),
+            1: (3, 1), 2: (3, 2), 3: (3, 3),
+            0: (4, 1), '.': (4, 2)
+        }
+
+        self.operations = {"/": "\u00F7", "*": "\00D7", "-": "-", "+": "+"}
